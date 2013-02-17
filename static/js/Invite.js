ff7a59c0a181bdcb0c32d8702b8db083f24a7f1f
Michael Helmbrecht <michael.helmbrecht@gmail.com>
2013-02-17 18:12:07 -0500

    Add User.fromJSON, create Invite

---
+++ static/js/Invite.js
@@ -0,0 +1,7 @@
+function Invite(userID, mealID) {
+	this.userID = userID;
+	this.mealID = mealID;
+}
+
+Invite.prototype.userID = -1;
+Invite.prototype.mealID = -1;
--- static/js/User.js
+++ static/js/User.js
@@ -3,12 +3,16 @@ $.getScript("/static/js/Utensils.js", function(){});
 $.getScript("/static/js/Meal.js", function(){});
 $.getScript("/static/js/Invite.js", function(){});
 
-function User(userName, passwowrd, name) {
+var userID = 0;
+
+function User(userName, password, name) {
+	this.id = userID++;
 	this.userName = userName;
 	this.password = password;
 	this.name = name;
 }
 
+User.prototype.id = -1;
 User.prototype.userName = "";
 User.prototype.password = "";
 User.prototype.name = "";
@@ -19,7 +23,6 @@ User.prototype.karma = 0;
 User.prototype.history = [];
 User.prototype.currentMeals = [];
 User.prototype.invites = [];
-User.prototype.dirpath = "../../../data/users";
 
 User.prototype.addIngredient = function(ingredient) {
 	this.ingredients.push(ingredient);
@@ -31,6 +34,18 @@ User.prototype.addFriend = function(friendID) {
 	this.friends.push(friendID);
 };
 
+User.prototype.fromJSON = function(data) {
+	this.id = data.id;
+	this.userName = data.userName;
+	this.name = data.name;
+	this.ingredients = data.ingredients;
+	this.friends = data.friends;
+	this.utensils = data.utensils;
+	this.karma = data.karma;
+	this.history = data.history;
+	this.currentMeals = data.currentMeals;
+	this.invites = data.invites;
+};
 
 /* Test User */
 var TEST_USER = new User("Test User");
