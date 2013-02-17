3a8e376dc9dfc3a77c51bcd6c19fd8171950b587
Neil Batlivala <neil.batlivala@gmail.com>
2013-02-17 18:31:41 -0500

    meal json functions

--- static/js/Meal.js
+++ static/js/Meal.js
@@ -17,3 +17,26 @@ Meal.prototype.name = "";
 Meal.prototype.recipe = null;
 Meal.prototype.userIds = [];
 Meal.prototype.startTime = null;
+Meal.prototype.fromJSON = function(data) {
+	if (data.id !== undefined) this.id = data.id;
+	if (data.ownerId !== undefined) this.ownerId = data.ownerId;
+	if (data.name !== undefined) this.name = data.name;
+	var recipe = new Recipe();
+	if (data.recipe !== undefined) this.recipe = recipe.fromJSON(data.recipe);
+	if (data.userIds !== undefined) this.userIds = data.userIds;
+	if (data.contributions !== undefined) this.contributions = JSON.parse(data.contributions);
+	if (data.startTime !== undefined) this.startTime = data.startTime;
+	if (data.endTime !== undefined) this.endTime = data.endTime;
+};
+Meal.prototype.toJSON = function() {
+	return JSON.stringify(this);
+	/*var res = {};
+	res.id = this.id;
+	res.ownerId = this.ownerId;
+	res.name = this.name;
+	res.recipe = this.recipe.toJSON();
+	res.userIds = data.userIds;
+	res.contributions = JSON.stringify(this.contributions);
+	res.startTime = this.startTime;
+	res.endTime = this.endTime;*/
+}
