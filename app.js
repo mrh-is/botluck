9d2f5eb6f86aa7bfcfd74bed58ff06d6f8d25e3a
Michael Helmbrecht <michael.helmbrecht@gmail.com>
2013-02-19 15:42:46 -0500

    Move HTML files into html folder

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ var mealCount = 0;
 var readUserData = function() {
 	fs.readFile(DBFile, function(err, data) {
     if (err) {
-    	userDB = {};
-    } else {
-     	var metadata = JSON.parse(data);
+        userDB = {};
+    }
+    else {
+        var metadata = JSON.parse(data);
         userDB = metadata.userDB;
         userCount = metadata.userCount;
         mealCount = metadata.mealCount;
@@ -41,7 +42,7 @@ var writeUserData = function() {
       console.log("Success writing file: ", DBFile);
     }
   });
-}
+};
 
 var checkPassword = function(username, password) {
 	if (userDB[username].password !== undefined &&
@@ -49,11 +50,11 @@ var checkPassword = function(username, password) {
 		return true;
 	}
 	return false;
-}
+};
 
 var changePassword = function(username, password) {
 	userDB[username].password = password;
-}
+};
 
 app.post("/verify/:url", function(request, response) {
 	var username = request.body.username;
@@ -74,14 +75,15 @@ app.get("/user/:id", function(request, response) {
 	var filename = userDir + "user-" + request.params.id + ".txt";
 	fs.readFile(filename, function(err, data) {
         if (err) {
-         	response.send({"success": false});
-         } else {
-         	var userData = JSON.parse(data);
-         	response.send({
+            response.send({"success": false});
+        }
+        else {
+            var userData = JSON.parse(data);
+            response.send({
                 "userData": userData,
                 "success": true
             });
-         }
+        }
     });
 });
 
@@ -89,10 +91,11 @@ app.post("/user/:id", function(request, response) {
 	var filename = userDir + "user-" + request.params.id + ".txt";
 	fs.writeFile(filename, request.body.data, function(err, data) {
         if (err) {
-         	response.send({"success": false});
-         } else {
-         	response.send({"success": true});
-         }
+            response.send({"success": false});
+        }
+        else {
+            response.send({"success": true});
+        }
     });
 });
 
@@ -106,7 +109,8 @@ app.post("/user", function(request, response) {
     console.log(userDB);
     if (userDB[username] !== undefined) {
         response.send({ "success": false });
-    } else {
+    }
+    else {
         userDB[username] = {
             "password": password,
             "id": id
@@ -133,14 +137,15 @@ app.get("/meal/:id", function(request, response) {
 	var filename = mealDir + "meal-" + request.params.id + ".txt";
 	fs.readFile(filename, function(err, data) {
         if (err) {
-         	response.send({"success": false});
-         } else {
-         	var mealData = JSON.parse(data);
-         	response.send({
+            response.send({"success": false});
+        }
+        else {
+            var mealData = JSON.parse(data);
+            response.send({
                 "success": true,
                 "mealData": mealData
             });
-         }
+        }
     });
 });
 
@@ -148,10 +153,11 @@ app.post("/meal/:id", function(request, response) {
 	var filename = mealDir + "meal-" + request.params.id + ".txt";
 	fs.writeFile(filename, request.body.data, function(err, data) {
         if (err) {
-         	response.send({"success": false});
-         } else {
-         	response.send({"success": true});
-         }
+            response.send({"success": false});
+        }
+        else {
+            response.send({"success": true});
+        }
     });
 });
 
@@ -167,11 +173,11 @@ app.get("/static/css/:filename", function (request, response) {
 
 // This is for serving home pages
 app.get("/static/home/meals/:filename", function (request, response) {
-	response.sendfile("static/home-meals-" + request.params.filename)
+	response.sendfile("static/html/home-meals-" + request.params.filename);
 });
 
 app.get("/static/home/:filename", function (request, response) {
-	response.sendfile("static/home-" + request.params.filename);
+	response.sendfile("static/html/home-" + request.params.filename);
 });
 
 // This is for serving files in the static directory
